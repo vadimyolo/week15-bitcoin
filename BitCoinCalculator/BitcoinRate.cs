40372f614cdce97f1564f5da67a88fce0b80d7a2
Opilane TTHK <opilane@tthk.ee>
2021-12-08 10:29:09 +0200

    bitcoin

---
+++ BitCoinCalculator/BitcoinRate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BitCoinCalculator
+{
+    class BitcoinRate
+    {
+        public bpi bpi { get; set; }
+        public string disclaimer { get; set; }
+    }
+
+    public class bpi
+    {
+        public USD USD { get; set; }
+    }
+
+
+    public class USD
+    {
+        public string code { get; set; }
+
+        public float rate_float { get; set; }
+    }
+}
---
+++ BitCoinCalculator/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace BitCoinCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BitcoinRate currentBitcoin = GetRates();
+            Console.WriteLine($"Current rate : {currentBitcoin.bpi.USD.code} {currentBitcoin.bpi.USD.rate_float}");
+            Console.WriteLine($"{currentBitcoin.disclaimer}");
+        }
+
+        public static BitcoinRate GetRates()
+        {
+            string url = "https://api.coindesk.com/v1/bpi/currentprice.json";
+
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            BitcoinRate bitcoinData;
+
+            using (var responseReader = new StreamReader(webStream))
+            {
+                var response = responseReader.ReadToEnd();
+                bitcoinData = JsonConvert.DeserializeObject<BitcoinRate>(response);
+            }
+            return bitcoinData;
+                
+        }
+    }
+}
